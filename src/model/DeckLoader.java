a73516329c4e5642ae3e1d6786d4c168ea0c9c03
unknown <b07902042@ntu.edu.tw>
2021-06-29 06:39:36 +0800

    pictures

--- src/model/DeckLoader.java
+++ src/model/DeckLoader.java
@@ -11,12 +11,31 @@ public class DeckLoader implements Serializable {
     
     public ArrayList<Card> loadDeck0(){
         ArrayList<Card> deck = new ArrayList<Card>();
-        for(int i = 0; i < 5; i++){
-            deck.add(new DrBoom());
-            deck.add(new HolyNova());
-            deck.add(new PenguinParty());
-
-        }
+        deck.add(new DrBoom());            
+        deck.add(new PenguinParty());
+        deck.add(new AlAkir());
+        deck.add(new Antonidas());
+        deck.add(new AlexStraza());
+        deck.add(new BlackKnight());
+        deck.add(new DeathWing());
+        deck.add(new DoomGuard());            
+        deck.add(new Fordring());
+        deck.add(new FriedChicken());
+        deck.add(new Hogger());
+        deck.add(new KingKrush());
+        deck.add(new KingMukla());
+        deck.add(new LichKing());
+        deck.add(new LightRagnaros());
+        deck.add(new Ragnaros());
+        deck.add(new Patches());
+        deck.add(new RenoJackson());
+        deck.add(new Tazdingo());
+        deck.add(new Thaurissan());
+        deck.add(new VanCleef());
+        deck.add(new VarianWrynn());
+        deck.add(new Penguin());
+        deck.add(new Penguinn());
+        deck.add(new PartyPenguin());
         return deck;
     }
 }
--- src/model/minion/Fordring.java
+++ src/model/minion/Fordring.java
@@ -2,18 +2,18 @@ package src.model.minion;
 import src.model.*;
 import java.util.List;
 //Complete
-public class Fording extends AbstractMinion implements Card, Minion, DeathRattle, DivineShield, Taunt{
+public class Fordring extends AbstractMinion implements Card, Minion, DeathRattle, DivineShield, Taunt{
     
-    private static String name = "Fording";
+    private static String name = "Fordring";
     private static String description = "DivineShield & Taunt & DeathRattle: Summon a 5/3 AshBringer";
     private static int baseCost = 8;
     private static int baseATK = 6;
     private static int baseHP = 6;
     private boolean divineShield = true;
     
-    public Fording (){
-        super(Fording.name, Fording.description, Fording.baseCost,
-                 Fording.baseHP, Fording.baseATK);    
+    public Fordring (){
+        super(Fordring.name, Fordring.description, Fordring.baseCost,
+                 Fordring.baseHP, Fordring.baseATK);    
     }
 
     @Override
