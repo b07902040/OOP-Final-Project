3dd379a471c1937989d29fbc1117767566808246
darwinze <pupss95103@gmail.com>
2021-06-30 06:21:47 +0800

    fix targeting issue on minion

--- heart/view/MinionPainter.java
+++ heart/view/MinionPainter.java
@@ -7,6 +7,7 @@ import java.util.List;
 import heart.constant.Const;
 import heart.model.GameInfo;
 import heart.model.Minion;
+import heart.model.Targeting;
 
 public class MinionPainter implements Painter {
     @Override
@@ -27,7 +28,7 @@ public class MinionPainter implements Painter {
             x = game.getMinionPosition(id, i)[0];
             y = game.getMinionPosition(id, i)[1];
             if ((game.isMyTurn() && minion.canAttack() && game.getState() == Const.STATE_PENDING) ||
-                (game.isMyTurn() && minion.canTargeted() && game.getState() == Const.STATE_CARD_TARGETING))
+                (game.isMyTurn() && minion.canTargeted() && game.getState() == Const.STATE_CARD_TARGETING && ((Targeting) game.getLastShowedCard()).getCandidates(game.getHero().getMaster()).contains(minion)))
                 View.drawValidMinionEffect(g, x, y, Const.MINION_W, Const.MINION_H);
             View.drawMinion(g, minion, x, y, 1);
 
@@ -41,7 +42,7 @@ public class MinionPainter implements Painter {
             x = game.getMinionPosition(opponentId, i)[0];
             y = game.getMinionPosition(opponentId, i)[1];
             if ((game.isMyTurn() && minion.canAttacked() && game.getState() == Const.STATE_ATTACKER_TARGETING) ||
-                (game.isMyTurn() && minion.canTargeted() && game.getState() == Const.STATE_CARD_TARGETING))
+                (game.isMyTurn() && minion.canTargeted() && game.getState() == Const.STATE_CARD_TARGETING && ((Targeting) game.getLastShowedCard()).getCandidates(game.getHero().getMaster()).contains(minion)))
                 View.drawValidMinionEffect(g, x, y, Const.MINION_W, Const.MINION_H);
             View.drawMinion(g, minion, x, y, 1);
         }
